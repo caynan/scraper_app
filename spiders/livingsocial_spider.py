3b775c6e2535d4464729f6372b30018d0195fbbf
Caynan <caynanvls@gmail.com>
2014-11-09 20:46:06 -0500

    Spider code

--- spiders/livingsocial_spider.py
+++ spiders/livingsocial_spider.py
@@ -5,8 +5,10 @@ from scrapy.contrib.loader.processor import Join, MapCompose
 
 from scraper_app.items import LivingSocialDeal
 
+
 class LivingSocialSpider(BaseSpider):
-    """Spider for regularly updated livingsocial.com site, San Francisco page """
+    """ Spider for regularly updated livingsocial.com site,
+        San Francisco page"""
     name = "livingsocial"
     allowed_domains = ["livingsocial.com"]
     start_urls = ["http://www.livingsocial.com/cities/15-san-francisco"]
@@ -20,3 +22,27 @@ class LivingSocialSpider(BaseSpider):
                        'original_price': './/a/div[@class="deal-bottom"]/ul[@class="unstyled deal-info"]/li[@class="deal-original"]/del/text()',
                        'price': './/a/div[@class="deal-bottom"]/ul[@class="unstyled deal-info"]/li[@class="deal-price"]/text()'}
 
+    def parse(self, response):
+        """
+        Default callback used by Scrapy to process downloaded responses
+
+        Testing contracts:
+        @url http://www.livingsocial.com/cities/15-san-francisco
+        @returns items 1
+        @scrapes title link
+
+        """
+        selector = HtmlXPathSelector(response)
+
+        # iterate over deals
+        for deal in selector.select(self.deals_list_xpath):
+            loader = XPathItemLoader(LivingSocialDeal(), selector=deal)
+
+            # define processors
+            loader.default_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor = Join()
+
+            # iterate over fields and add xpaths to the loader
+            for field, xpath in self.item_fields.iteritems():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
